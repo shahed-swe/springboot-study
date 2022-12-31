c4ed652ae694f857a81ec2a00be995dccf4632de
Shahed Talukder <shahedtalukder51@gmail.com>
2022-12-31 22:31:56 +0600

    new commit

--- studentmanagement/src/main/java/com/studentmanagement/studentmanagement/StudentmanagementApplication.java
+++ studentmanagement/src/main/java/com/studentmanagement/studentmanagement/StudentmanagementApplication.java
@@ -2,12 +2,15 @@ package com.studentmanagement.studentmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class StudentmanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentmanagementApplication.class, args);
+//		taker
 	}
+
 
 }
